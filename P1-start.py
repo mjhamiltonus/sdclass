011ac31218da6c91859285c03052edde502ed21d
Michael Hamilton <hamilton@nurc.nato.int>
2017-07-22 16:12:26 -0700

    Updated line detect parameters to catch more of dashed lines on highway.

--- P1-start.py
+++ P1-start.py
@@ -250,8 +250,8 @@ for ii in range(len(procImages)):
     rho = 1 # distance resolution in pixels of the Hough grid
     theta = np.pi/180 # angular resolution in radians of the Hough grid
     threshold = 3     # minimum number of votes (intersections in Hough grid cell)
-    min_line_length = 30 #minimum number of pixels making up a line
-    max_line_gap    = 10 # maximum gap in pixels between connectable line segments
+    min_line_length = 25 # minimum number of pixels making up a line
+    max_line_gap    = 15 # maximum gap in pixels between connectable line segments
     line_image = np.copy(image)*0 # creating a blank to draw lines on
 
     # Run Hough on edge detected image
